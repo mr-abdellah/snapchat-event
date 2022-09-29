f549b6ccec351f61e092ac598cd993ffe4135233
mr-abdellah <belkaid.abdelah@gmail.com>
2022-09-29 11:14:53 +0100

    Add responsive homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import Homepage from "./pages/Homepage";
+import "./app.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/create",
+    element: <div> create page</div>,
+  },
+  {
+    path: "/event",
+    element: <div> event page</div>,
+  },
+]);
+
 function App() {
   return (
-    <div>
-      Hello world!
+    <div className="app">
+      <RouterProvider router={router} />
     </div>
   );
 }
